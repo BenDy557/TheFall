bb31a3813bd020c65caf54732d7c26584a998a61
Ben Doherty <1103410@live.abertay.ac.uk>
2016-02-05 15:34:56 +0000

    Added pickup Spawning

---
+++ Fall/Assets/Scripts/Pickups/PickupSpawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupSpawn : MonoBehaviour {
+
+    public GameObject m_Pickup;
+    public float m_Probability;
+
+	// Use this for initialization
+	void Start () {
+
+        float tempProbability = Random.Range(0.0f, 1.0f);
+        Debug.Log(tempProbability);
+        if (tempProbability < m_Probability)
+        {
+            Instantiate(m_Pickup, transform.position, new Quaternion());
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
