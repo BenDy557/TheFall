7a503c571a4bd85365ba2bf455e63ebbe13623e5
Glenn Cullen <1100862@MC10794.uad.ac.uk>
2016-02-12 16:47:43 +0000

    midbuild

---
+++ Fall/Assets/Scripts/Managers/WinManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void IWon(string WinnerName)
+	{
+		Debug.Log (WinnerName + " won!");
+
+		Application.LoadLevel(0);
+		
+	}
+}
