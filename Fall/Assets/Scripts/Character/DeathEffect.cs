00c5b2ccf1e9b7748099c018e3266da62c26061b
Ben Doherty <1103410@live.abertay.ac.uk>
2016-02-19 17:49:58 +0000

    Added Sounds, fixed speed limit bug

---
+++ Fall/Assets/Scripts/Character/DeathEffect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathEffect : MonoBehaviour {
+
+    [HideInInspector]  public AudioSource m_AudioSource;
+    public AudioClip m_AudioClipDeath;
+
+	// Use this for initialization
+	void Start () {
+
+        m_AudioSource = GetComponent<AudioSource>();
+        StartCoroutine(DeathEffectTimer());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+
+
+	}
+
+    IEnumerator DeathEffectTimer()
+    {
+        m_AudioSource.PlayOneShot(m_AudioClipDeath);
+        yield return new WaitForSeconds(m_AudioClipDeath.length);
+        Destroy(gameObject);
+    }
+}
+
