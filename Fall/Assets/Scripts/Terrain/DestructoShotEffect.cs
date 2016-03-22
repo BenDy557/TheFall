88bd19f5a77f8b7cd89dad364956f23f30bb543b
Ben Doherty <1103410@live.abertay.ac.uk>
2016-03-22 14:56:01 +0000

    fixed destructo shot effect

--- Fall/Assets/Scripts/Terrain/DestructoShotEffect.cs
+++ Fall/Assets/Scripts/Terrain/DestructoShotEffect.cs
@@ -1,35 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class DestructoShotEffect : MonoBehaviour {
 
-    private Material m_DefaultMaterial;
+    public List<GameObject> m_Meshes = new List<GameObject>();
+    public List<Material> m_DefaultMaterials = new List<Material>();
+
     public Material m_PhaseMaterial;
     float m_Duration = 3.0f;
     bool m_Enabled = false;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 
-        m_DefaultMaterial = new Material(gameObject.GetComponent<MeshRenderer>().material);
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        for (int i = 0; i < m_Meshes.Count; i++)
+        {
+            m_DefaultMaterials.Add(m_Meshes[i].GetComponent<Renderer>().material);
+        }
 
         gameObject.GetComponent<Collider>().isTrigger = false;
-        gameObject.GetComponent<MeshRenderer>().material = m_DefaultMaterial;
-        gameObject.tag = "Ground";
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //gameObject.GetComponent<Collider>().isTrigger = false;
+        //gameObject.GetComponent<MeshRenderer>().material = m_DefaultMaterial;
+        //gameObject.tag = "Ground";
 
         if (m_Enabled)
         {
             //start animation
             //disable collider
-            gameObject.GetComponent<Collider>().isTrigger = true; ;
-            gameObject.tag = "PhasedOut";
-            gameObject.GetComponent<MeshRenderer>().material = m_PhaseMaterial;
+                        
             //gameObject.GetComponent<MeshRenderer>().material.color = new Color(tempColor.r, tempColor.g, tempColor.b, 0.0f);
-
         }
         
 	}
@@ -42,7 +48,24 @@ public class DestructoShotEffect : MonoBehaviour {
     IEnumerator WaitDuration()
     {
         m_Enabled = true;
+
+        for (int i = 0; i < m_Meshes.Count; i++)
+        {
+            m_Meshes[i].GetComponent<Renderer>().material = m_PhaseMaterial;
+        }
+        gameObject.GetComponent<Collider>().isTrigger = true;
+        gameObject.tag = "PhasedOut";
+
         yield return new WaitForSeconds(m_Duration);
+
+        for (int i = 0; i < m_Meshes.Count; i++)
+        {
+            m_Meshes[i].GetComponent<Renderer>().material = m_DefaultMaterials[i];
+        }
+        gameObject.GetComponent<Collider>().isTrigger = false;
+        gameObject.tag = "Ground";
+
         m_Enabled = false;
     }
 }
+
